9be15bd218e1f4feae3ab5b50e2a6b4dbe1b21e0
Josemruizbros <josemanuelruizpalacios1@gmail.com>
2026-05-11 11:12:27 +0200

    archivo para crear un usuario de prueba de cada rol

--- backend/create_admin.py
+++ backend/create_admin.py
@@ -3,12 +3,26 @@ from security import hash_password # Tu función de encriptar contraseñas
 import models
 
 db = SessionLocal()
-admin_user = models.Usuario(
-    nombre="Admin General",
+admin = models.Usuario(
+    nombre="Admin",
     email="admin@instituto.es",
     password_hash=hash_password("admin123"), # Usa tu función de hash
     rol="Administrador" # Asegúrate de que coincida con tus roles
 )
-db.add(admin_user)
+profe = models.Usuario(
+    nombre="Profe",
+    email="profe@instituto.es",
+    password_hash=hash_password("profe123"), # Usa tu función de hash
+    rol="Profesor" # Asegúrate de que coincida con tus roles
+)
+alumno = models.Usuario(
+    nombre="Alumno",
+    email="alumno@instituto.es",
+    password_hash=hash_password("alumno123"), # Usa tu función de hash
+    rol="Alumno" # Asegúrate de que coincida con tus roles
+)
+db.add(admin)
+db.add(profe)
+db.add(alumno)
 db.commit()
-print("¡Administrador creado con éxito!")
+print("¡Usuarios creados con éxito!")
