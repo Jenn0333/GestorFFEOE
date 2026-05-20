46bc3cdc1086a5476b38189319780e4ed9c8722d
Josemruizbros <josemanuelruizpalacios1@gmail.com>
2026-05-20 12:02:58 +0200

    nuevo test de crear usuarios

--- backend/create_admin.py
+++ backend/create_admin.py
@@ -3,40 +3,21 @@ from security import hash_password # Tu función de encriptar contraseñas
 import models
 
 db = SessionLocal()
-admin = models.Usuario(
-    id = 1,
-    nombre="Admin",
-    email="admin@instituto.es",
-    password_hash=hash_password("admin123"),
-    rol="admin"
-)
-profe = models.Usuario(
-    id = 2,
-    nombre="Profe",
-    email="profe@instituto.es",
-    password_hash=hash_password("profe123"),
-    rol="profesor"
-)
-usuario = models.Usuario(
-    id = 3,
-    nombre="Alumno",
-    email="alumno@instituto.es",
-    password_hash=hash_password("alumno123"),
-    rol="alumno"
-)
-ciclo = models.Ciclo(
-    nombre = "DAM",
-    anio_inicio = 2025,
-    anio_fin = 2027
-)
-alumno = models.Alumno(
-    usuario_id = 3,
-    ciclo_id = 2
-)
+
+# Modificación en create_admin.py (quita los "id = X")
+admin = models.Usuario(nombre="Admin", email="admin@instituto.es", password_hash=hash_password("admin123"), rol="admin")
+profe = models.Usuario(nombre="Profe", email="profe@instituto.es", password_hash=hash_password("profe123"), rol="profesor")
+usuario = models.Usuario(nombre="Alumno", email="alumno@instituto.es", password_hash=hash_password("alumno123"), rol="alumno")
+
 db.add(admin)
 db.add(profe)
 db.add(usuario)
+db.commit()
+
+ciclo = models.Ciclo(nombre="DAM", anio_inicio=2025, anio_fin=2027)
 db.add(ciclo)
+db.commit()
+
+alumno = models.Alumno(usuario_id=usuario.id, ciclo_id=ciclo.id)
 db.add(alumno)
 db.commit()
-print("¡Usuarios creados con éxito!")
